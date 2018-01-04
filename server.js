d6b7aec8e0e04b9cccdacf4928eb73fa7caa62e6
Juan David <juandlsanta@gmail.com>
2018-01-04 14:42:38 -0500

    var port = process.env.PORT || 8000

--- server.js
+++ server.js
@@ -20,9 +20,16 @@
 require('dotenv').config({silent: true});
 
 var server = require('./app');
-var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+//var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+var port = process.env.PORT || 8000
 
-server.listen(port, function() {
+
+/* server.listen(port, function() {
   // eslint-disable-next-line
   console.log('Server running on port: %d', port);
+}); 
+*/
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
 });
